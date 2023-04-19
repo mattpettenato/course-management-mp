1808443ae4793edef785e1d8e104c35e161c2526
mattpettenato <mattpettenato@gmail.com>
2023-04-18 17:01:06 -0700

    i think this is working now??????
    python is silly i miss ruby on rails

--- __init__.py
+++ __init__.py
@@ -1,23 +1,10 @@
-# from . import routes
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-# import config
-
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost:5432/golfcourse'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# db = SQLAlchemy(app)
-
+from . import routes
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import config
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost:5432/golfcourse'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
-
-from routes import users
-
-if __name__ == '__main__':
-    app.run()
